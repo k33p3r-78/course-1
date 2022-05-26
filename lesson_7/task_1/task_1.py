dc7b2462c981cb32fef39adc8d27e9e042594544
Игорь Демьяненко <cmp.int0x80@gmail.com>
2022-05-26 20:12:01 +0300

    task 1

---
+++ lesson_7/task_1/task_1.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+
+starter = {
+    'my_project': [
+        'settings',
+        'mainapp',
+        'adminapp',
+        'authapp'
+    ]
+}
+
+for root_folder, sub_folder in starter.items():
+    
+
+
