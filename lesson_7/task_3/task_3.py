9666560a8280393c9f0ab6dad0f96692c13a0fb5
Игорь Демьяненко <cmp.int0x80@gmail.com>
2022-06-01 10:28:29 +0300

    task 3

--- lesson_7/task_3/task_3.py
+++ lesson_7/task_3/task_3.py
@@ -7,5 +7,8 @@ project_path = Path().resolve().joinpath('my_project')
 
 for root, dirs, files in os.walk(project_path):
     if root.endswith('templates'):
-        shutil.copytree(root, project_path.joinpath('templates'), dirs_exist_ok=True)        
+        try:
+            shutil.copytree(root, project_path.joinpath('templates'), dirs_exist_ok=True)
+        except OSError as e:
+            print(e)
 
