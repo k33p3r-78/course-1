4ed23cb168d2a919a86d8767bfbfd359435175d8
Игорь Демьяненко <cmp.int0x80@gmail.com>
2022-06-06 14:32:07 +0300

    task 1

--- lesson_9/task_1.py
+++ lesson_9/task_1.py
@@ -5,12 +5,15 @@ import time
 class TrafficLight:
 
     def __init__(self, red_time, yellow_time, green_time):
-        self.__color = 'красный'
+        self.change_color('красный')
         self._color_sleep = {
             'красный': red_time,
             'жёлтый': yellow_time,
             'зелёный': green_time
         }
+
+    def change_color(self, color):
+        self.__color = color
 
     def state(self):
         return self.__color
@@ -18,7 +21,7 @@ class TrafficLight:
     def running(self):
         color_queue = itertools.cycle(['красный', 'жёлтый', 'зелёный', 'жёлтый'])
         for color in color_queue:
-            self.__color = color
+            self.change_color(color)
             print(self.state())
             try:
                 time.sleep(self._color_sleep[color])
